732da591e18c2764d5f48ea5044cd86d978e5903
univernik77 <146747152+univernik77@users.noreply.github.com>
2023-10-30 17:21:15 +0300

    Добавлен код для создания папки

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import requests
+from pathlib import Path
+
 
 def download_picture():
     filename = 'hubble.jpeg'
+    directory = 'images'
     url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 
+    Path(directory).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(filename, 'wb') as file:
+    with open(f'{directory}/{filename}', 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == '__main__':
     download_picture()
-
-
