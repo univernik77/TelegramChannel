c91458e9d99a6902f75937093836b43930bddcba
univernik77 <146747152+univernik77@users.noreply.github.com>
2023-10-30 17:38:49 +0300

    Изменена функция для добавления картинок

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import requests
 from pathlib import Path
 
 
-def download_picture():
-    filename = 'hubble.jpeg'
-    directory = 'images'
-    url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+def download_picture(url, path):
 
-    Path(directory).mkdir(parents=True, exist_ok=True)
+    Path(path.split('/')[0]).mkdir(parents=True, exist_ok=True)
     response = requests.get(url)
     response.raise_for_status()
 
-    with open(f'{directory}/{filename}', 'wb') as file:
+    with open(path, 'wb') as file:
         file.write(response.content)
 
 
 if __name__ == '__main__':
-    download_picture()
+    path_to_file = 'images/hubble.jpeg'
+    outer_url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+
+    download_picture(outer_url, path_to_file)
